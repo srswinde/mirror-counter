3d545ca836627da11d49b43056d041bc1e9a9d86
scott <scottswindell@email.arizona.edu>
2018-07-22 20:55:08 +0000

    slight change to install

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-
+apt-get remove --purge apache2
 apt-get install nginx
 apt-get install php7.0-fpm
 apt-get install postgresql-server-dev-9.6
+
 cd db
 su postgres
-cd db
 psql -f setup.sql
 cp default /etc/nginx/sites-available/default
 
